1645ec083fced18f009ea3256721affc90261a0a
Adnan-Android <adnan.maqbool@axelliant.com>
2024-05-13 18:41:00 +0500

    archetecture

--- app/src/main/java/com/axelliant/android_erp/screens/MainActivity.kt
+++ app/src/main/java/com/axelliant/android_erp/screens/MainActivity.kt
@@ -26,6 +26,7 @@ class MainActivity : AppCompatActivity() {
         ).show()
 
 
+
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
