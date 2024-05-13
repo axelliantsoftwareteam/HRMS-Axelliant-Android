b035cf835b46e17168a99c30d96cdffa2675f2a0
Adnan-Android <adnan.maqbool@axelliant.com>
2024-05-13 15:27:06 +0500

    simple activity creation

---
+++ app/src/main/java/com/axelliant/android_erp/screens/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.axelliant.android_erp.screens
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.axelliant.android_erp.R
+
+class MainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
