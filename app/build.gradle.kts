4ef7af27985cb7b039b1097004efedfc92e838ff
arslan <arslan.umar@khazanapk.com>
2024-05-13 18:01:30 +0500

    setting the styles and fonts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
 }
 
 android {
@@ -33,6 +34,11 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        viewBinding =true
+        dataBinding =true
+    }
 }
 
 dependencies {
@@ -43,4 +49,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // text size and dimentions
+    implementation("com.intuit.ssp:ssp-android:1.1.0") // multi screen text sizes support
+    implementation("com.intuit.sdp:sdp-android:1.1.0") // multi screen width height support
+
 }
