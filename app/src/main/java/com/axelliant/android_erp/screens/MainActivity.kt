7909083b7076a40cf012c593d43be0c920c6f245
Adnan-Android <adnan.maqbool@axelliant.com>
2024-05-13 15:28:08 +0500

    design first commit

--- app/src/main/java/com/axelliant/android_erp/screens/MainActivity.kt
+++ app/src/main/java/com/axelliant/android_erp/screens/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.axelliant.android_erp.R
 
+
 class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
