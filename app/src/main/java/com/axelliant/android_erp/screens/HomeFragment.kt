44dabba7f03e76f73715ad463705c3b5e367d60e
Adnan-Android <adnan.maqbool@axelliant.com>
2024-05-15 15:51:48 +0500

    location done timer done

--- app/src/main/java/com/axelliant/android_erp/screens/HomeFragment.kt
+++ app/src/main/java/com/axelliant/android_erp/screens/HomeFragment.kt
@@ -1,13 +1,23 @@
 package com.axelliant.android_erp.screens
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.axelliant.android_erp.Test
 import com.axelliant.android_erp.adapter.WeeklyAdapter
@@ -16,11 +26,74 @@ import com.axelliant.android_erp.callback.AdapterItemClick
 import com.axelliant.android_erp.databinding.FragmentHomeBinding
 import com.axelliant.android_erp.extention.showSuccessMsg
 import com.axelliant.android_erp.utils.Utils.getCurrentDate
+import java.io.IOException
+import java.util.Locale
 
 class HomeFragment : BaseFragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding
+
+    private var currentLocation: Location? = null
+    private lateinit var locationManager: LocationManager
+
+
+    private val gpsLocationListener: LocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            currentLocation = location
+            setCurrentLocationText()
+        }
+
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+        override fun onProviderEnabled(provider: String) {}
+        override fun onProviderDisabled(provider: String) {}
+    }
+
+    //------------------------------------------------------//
+    private val networkLocationListener: LocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            currentLocation = location
+            setCurrentLocationText()
+        }
+
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+        override fun onProviderEnabled(provider: String) {}
+        override fun onProviderDisabled(provider: String) {}
+    }
+
+    private fun setCurrentLocationText(){
+        binding?.tvLocTxt?.text = getLocationAddress(currentLocation)
+
+    }
+
+    private fun getLocationAddress(location: Location?): String {
+        if (location == null) {
+            return "Location not available"
+        }
+        if (!Geocoder.isPresent()) {
+            return "Geocoding not supported"
+        }
+        val geocoder = Geocoder(requireContext(), Locale.getDefault())
+        try {
+            val addresses: List<Address> =
+                geocoder.getFromLocation(location.latitude, location.longitude, 1)!!
+            if (addresses.isNotEmpty()) {
+                val address: Address = addresses[0]
+
+                val sb = StringBuilder()
+                sb.append(address.thoroughfare ?: "").append(address.subThoroughfare ?: "")
+                for (i in 0 until address.maxAddressLineIndex) {
+                    sb.append(address.getAddressLine(i)).append(", ")
+                }
+                sb.append(address.locality).append(", ")
+                sb.append(address.countryName)
+                return sb.toString()
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return "NA"
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,6 +106,7 @@ class HomeFragment : BaseFragment() {
     }
 
 
+    @SuppressLint("MissingPermission")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -46,7 +120,32 @@ class HomeFragment : BaseFragment() {
                 }
 
                 if (allAreGranted) {
-//                    requireContext().showSuccessMsg("Location permission granted")
+                    locationManager =
+                        requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+                    val hasGps = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                    val hasNetwork =
+                        locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+
+                    if (hasGps) {
+                        locationManager.requestLocationUpdates(
+                            LocationManager.GPS_PROVIDER,
+                            5000,
+                            0F,
+                            gpsLocationListener
+                        )
+                    }
+                    if (hasNetwork) {
+                        locationManager.requestLocationUpdates(
+                            LocationManager.NETWORK_PROVIDER,
+                            5000,
+                            0F,
+                            networkLocationListener
+                        )
+                    }
+
+
+
                 } else {
 //                    requireContext().showSuccessMsg("Ask Location permission")
 
@@ -63,9 +162,28 @@ class HomeFragment : BaseFragment() {
         dataPopulate()
 
         binding?.tvDateTxt?.text = getCurrentDate()
+        binding?.textClock?.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(editable: Editable?) {
+                if (editable.toString().length >= 3) {
+                    binding?.tvHour?.text = editable?.split(":")?.get(0)
+                    binding?.tvMin?.text = editable?.split(":")?.get(1)
+                    binding?.tvSec?.text = editable?.split(":")?.get(2)
+
+                }
+
+            }
+
+        })
 
     }
 
+    override fun onPause() {
+        super.onPause()
+    }
 
     private fun dataPopulate() {
         binding?.rvWeekly?.layoutManager = LinearLayoutManager(requireActivity())
