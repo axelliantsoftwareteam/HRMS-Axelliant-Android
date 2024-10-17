191ecdcf9bc6f3723a01926ca3b7fa57f0eaf1af
MuhammadArslanUmar <arslan.umar@axelliant.com>
2024-10-17 01:52:51 -0700

    update the ImageView to fit the screen according to the size #36

--- app/src/main/java/com/axelliant/hris/adapter/ImagePagerAdapter.kt
+++ app/src/main/java/com/axelliant/hris/adapter/ImagePagerAdapter.kt
@@ -9,6 +9,7 @@ import androidx.viewpager.widget.PagerAdapter
 import com.axelliant.hris.R
 import com.axelliant.hris.extention.setUrlImage
 import com.axelliant.hris.model.expense.Attachments
+import com.bumptech.glide.Glide
 
 
 class ImagePagerAdapter(private val context: Context, private val images: List<Attachments>) :
@@ -22,10 +23,12 @@ class ImagePagerAdapter(private val context: Context, private val images: List<A
         val view = inflater.inflate(R.layout.image_detail_row, container, false)
 
         val imageView = view.findViewById<ImageView>(R.id.imageView)
-
-        imageView.setUrlImage(
-            images[position].file_url, context
-        )
+        Glide.with(context)
+            .load(images[position].file_url) // image url
+            .placeholder(R.drawable.ic_place_holder) // any placeholder to load at start
+            .error(R.drawable.ic_place_holder)  // any image in case of error
+            .centerCrop()
+            .into(imageView)
 
 
         container.addView(view)
