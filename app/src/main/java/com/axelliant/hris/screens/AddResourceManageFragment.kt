549c842d133415102338759b535a6238864d70bc
MuhammadArslanUmar <arslan.umar@axelliant.com>
2024-12-26 03:48:19 -0800

    Create Screen and design #58

---
+++ app/src/main/java/com/axelliant/hris/screens/AddResourceManageFragment.kt
@@ -0,0 +1,21 @@
+package com.axelliant.hris.screens
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.axelliant.hris.R
+
+
+class AddResourceManageFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_resource_manage, container, false)
+    }
+
+}
