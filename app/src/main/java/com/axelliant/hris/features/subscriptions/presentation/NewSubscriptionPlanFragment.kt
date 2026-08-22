5c79eaad466e44d28bb7a0c35f7f994dd891463b
Arslan-Android <arslan.umar@axelliant.com>
2026-08-22 13:53:13 -0700

    Repair new subscription plan layout

--- app/src/main/java/com/axelliant/hris/features/subscriptions/presentation/NewSubscriptionPlanFragment.kt
+++ app/src/main/java/com/axelliant/hris/features/subscriptions/presentation/NewSubscriptionPlanFragment.kt
@@ -22,7 +22,6 @@ class NewSubscriptionPlanFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) = with(binding) {
         appTopBar.titleView.textSize = 14f
         appTopBar.setOnBackClickListener { findNavController().navigateUp() }
-        appTopBar.setOnActionClickListener { requireContext().showSuccessMsg(getString(R.string.subscriptions_more_actions_coming_soon)) }
         cancelButton.setOnClickListener { findNavController().navigateUp() }
         createPlanButton.setOnClickListener {
             requireContext().showSuccessMsg(getString(R.string.subscription_plan_created))
